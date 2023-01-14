5ed913bfee34299e7a00bf2073a1e0f1293e3cd5
Joseph LaFreniere <joseph@lafreniere.xyz>
2023-01-13 20:44:49 -0600

    Add license and Nix flake config

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Source code for lafreniere.xyz";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/master;
+    flake-utils.url = github:numtide/flake-utils;
+    pre-commit-hooks = {
+      url = github:cachix/pre-commit-hooks.nix;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+    with flake-utils.lib; eachSystem allSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        buildInputs = with pkgs; [
+          terraform
+          terragrunt
+        ];
+      in
+      rec {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              typos.enable = true;
+            };
+          };
+        };
+        devShell = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = buildInputs ++ (with pkgs; [
+            nixfmt
+            typos
+          ]);
+        };
+      }
+    );
+}
